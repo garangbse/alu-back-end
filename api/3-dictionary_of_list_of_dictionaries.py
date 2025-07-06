a3f337bbcd4c3489974447c39d325deba0eb00fa
Garangbse <g.buke@alustudent.com>
2025-07-06 16:45:33 +0200

    correcting spacing

--- api/3-dictionary_of_list_of_dictionaries.py
+++ api/3-dictionary_of_list_of_dictionaries.py
@@ -11,26 +11,26 @@ def export_all_employees_todo():
     users_url = "https://jsonplaceholder.typicode.com/users"
     users_response = requests.get(users_url)
     users = users_response.json()
-    
+
     # Get all todos
     todos_url = "https://jsonplaceholder.typicode.com/todos"
     todos_response = requests.get(todos_url)
     todos = todos_response.json()
-    
+
     # Create dictionary to store all employee data
     all_employees = {}
-    
+
     # Process each user
     for user in users:
         user_id = str(user.get('id'))
         username = user.get('username')
-        
+
         # Get todos for this user
         user_todos = []
         for todo in todos:
             if todo.get('userId') == user.get('id'):
                 user_todos.append(todo)
-        
+
         # Format todos for this user
         formatted_todos = []
         for todo in user_todos:
@@ -41,7 +41,7 @@ def export_all_employees_todo():
             })
 
         all_employees[user_id] = formatted_todos
-    
+
     # Write to JSON file
     with open('todo_all_employees.json', 'w') as jsonfile:
         json.dump(all_employees, jsonfile)
@@ -49,4 +49,3 @@ def export_all_employees_todo():
 
 if __name__ == "__main__":
     export_all_employees_todo()
-
